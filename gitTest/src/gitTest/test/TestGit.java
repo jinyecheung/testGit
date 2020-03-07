f5b87062ab0adb7eab49627e800966b8e0b886e1
Administrator <Administrator@BBZU3VZ7NFPAY55>
2020-03-07 11:55:09 +0800

    created by JININK at 2020.03.07 11:54 am,For test Git

---
+++ gitTest/src/gitTest/test/TestGit.java
@@ -0,0 +1,17 @@
+package gitTest.test;
+
+
+/**
+* @author Jinink Cheung
+* @date   2020年3月7日-上午11:51:35
+*/
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("TestGit.main()");
+		System.out.println("Hello Git");
+	}
+}
+
+
