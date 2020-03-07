079f340234625d3f4b164186f20eebe14c0e548e
Administrator <Administrator@BBZU3VZ7NFPAY55>
2020-03-07 16:29:07 +0800

    4

--- gitTest/src/gitTest/test/TestGit.java
+++ gitTest/src/gitTest/test/TestGit.java
@@ -5,6 +5,9 @@ package gitTest.test;
 * @author Jinink Cheung
 * @date   2020年3月7日-上午11:51:35
 * https://github.com/jinyecheung/testGit
+* 
+* 克隆仓库 ：
+* https://github.com/benwang6/spring-cloud-repo
 */
 
 public class TestGit implements mygit {
